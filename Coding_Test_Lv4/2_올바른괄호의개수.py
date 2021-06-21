a09ae078c9432480678b8fd627049928b150738f
Do-code-ing <zxcvbn658@naver.com>
2021-06-21 17:34:44 +0900

    2021-06-21

---
+++ Coding_Test_Lv4/2_올바른괄호의개수.py
@@ -0,0 +1,16 @@
+from math import factorial
+
+def solution(n):
+    numer = factorial(2*n) // (n+1)
+    fact_n = factorial(n)
+    deno = fact_n * fact_n
+    
+    return numer // deno
+
+# 1 = 1
+# 2 = 2
+# 3 = 5
+# 4 = 14
+# ...
+# 카탈란 수 구하기
+# Cn = (2n)! / n!n! / n+1
