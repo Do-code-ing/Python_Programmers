156970cdc5069a4b04c8edcdc2f4a77aaef348e3
Do-code-ing <zxcvbn658@naver.com>
2021-06-26 18:39:00 +0900

    2021-06-26

--- Coding_Test_Lv4/15_미완_2017팁스다운_단어퍼즐.py
+++ Coding_Test_Lv4/15_미완_2017팁스다운_단어퍼즐.py
@@ -1,21 +1,20 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12983?language=python3
 
 def solution(strs, t):
-    answer = float('inf')
     n = len(t)
-    m = len(strs)
+    dp = [0] * (n+1)
+    strs = set(strs) # 중복 문자열 제거
     
-    def btk(start, value):
-        nonlocal answer
-        if start == n:
-            if answer > value:
-                answer = value
-            return
-        
-        for char in strs:
-            if t[start:].startswith(char):
-                btk(start + len(char), value + 1)
-
-    btk(0, 0)
-
-    return answer if answer != float('inf') else -1
+    for i in range(1, n+1): # t[s:i]를 위해 1부터 시작
+        dp[i] = float('inf') # 일단 무한으로 갱신
+        for k in range(1, 6): # strs안에 있는 각 문자열의 길이가 1이상 5이하이므로 
+            if i - k < 0: # 시작 인덱스가 음수가 되버린다면 0으로
+                s = 0
+            else:
+                s = i - k
+            if t[s:i] in strs:
+                value = dp[i-k] + 1
+                if dp[i] > value:
+                    dp[i] = value
+    
+    return dp[-1] if dp[-1] != float('inf') else -1
