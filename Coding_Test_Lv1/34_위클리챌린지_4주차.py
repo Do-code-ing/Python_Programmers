8af97206e0edb37b2ad52729cc4f50df79b745e8
Do-code-ing <zxcvbn658@naver.com>
2021-08-27 21:10:53 +0900

    2021-08-27

---
+++ Coding_Test_Lv1/34_위클리챌린지_4주차.py
@@ -0,0 +1,22 @@
+# https://programmers.co.kr/learn/courses/30/lessons/84325
+
+def solution(table, languages, preference):
+    max_job = "Z"
+    max_score = 0
+    for arr in table:
+        job, *arr = arr.split()
+        score = 0
+        for i in range(len(arr)):
+            for j in range(len(languages)):
+                if arr[i] == languages[j]:
+                    score += (5-i) * preference[j]
+        
+        if max_score < score:
+            max_score = score
+            max_job = job
+        
+        elif max_score == score:
+            if max_job > job:
+                max_job = job
+    
+    return max_job
